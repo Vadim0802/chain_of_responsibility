6f9b1df5fec6272fedcb112cde98ea021dceeed5
bodia <v.bodyansky@yandex.ru>
2020-10-20 19:49:44 +1000

    init

---
+++ Handler.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.IO;
+using System.Linq;
+
+namespace Server
+{
+    abstract class Handler
+    {
+        public Handler Successor { get; set; }
+        public abstract void HandlerRequest(HttpListenerContext context);
+    }
+
+    class StreamHandler : Handler
+    {
+        public override void HandlerRequest(HttpListenerContext context)
+        {
+            HttpListenerRequest request = context.Request;
+            string query = request.QueryString.GetKey(0);
+
+            if (query == "text")
+            {
+                HttpListenerResponse response = context.Response;
+
+                string bodyText = $"<html><head><meta charset='utf8'></head><body>{request.QueryString.Get(query)}</body></html>";
+
+                byte[] byteBodyText = Encoding.UTF8.GetBytes(bodyText);
+                response.ContentLength64 = byteBodyText.Length;
+
+                using(Stream output = response.OutputStream)
+                {
+                    output.Write(byteBodyText, 0, byteBodyText.Length);
+                }
+            }
+            else if (Successor != null)
+            {
+                Successor.HandlerRequest(context);
+            }
+        }
+    }
+
+    class ConvertHandler : Handler
+    {
+        public override void HandlerRequest(HttpListenerContext context)
+        {
+            HttpListenerRequest request = context.Request;
+            string query = request.QueryString.GetKey(0);
+
+            if (query == "convert")
+            {
+                HttpListenerResponse response = context.Response;
+                string valueQueryString = request.QueryString.Get(query);
+                string[] bytesString = Encoding.UTF8.GetBytes(valueQueryString).Select((byte item) => $"{item}").ToArray();
+
+                string bodyText = $"<html><head><meta charset='utf8'></head><body>{String.Join("", bytesString)}</body></html>";
+                byte[] byteBodyText = Encoding.UTF8.GetBytes(bodyText);
+
+                response.ContentLength64 = byteBodyText.Length;
+
+                using (Stream output = response.OutputStream)
+                {
+                    output.Write(byteBodyText, 0, byteBodyText.Length);
+                }
+            }
+            else if (Successor != null)
+            {
+                Successor.HandlerRequest(context);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Listen();
+            Console.ReadKey();
+        }
+
+        private static void Listen()
+        {
+            using (HttpListener listener = new HttpListener())
+            {
+                listener.Prefixes.Add("http://localhost:8888/connection/");
+                listener.Start();
+                Console.WriteLine("Ожидание подключений...");
+
+                while (true)
+                {
+                    HttpListenerContext context = listener.GetContext();
+
+                    var handler = InitChain();
+                    handler.HandlerRequest(context);
+                }
+            }
+        }
+
+        private static StreamHandler InitChain()
+        {
+            StreamHandler sh = new StreamHandler();
+            ConvertHandler ch = new ConvertHandler();
+
+            sh.Successor = ch;
+
+            return sh;
+        }
+    }
+}
